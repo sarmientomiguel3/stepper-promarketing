10a6e64540e9b4ca0c1bd335478c0e3b80294416
sarmientomiguel3 <sarmientomiguel3@gmail.com>
2023-07-24 23:44:14 -0500

    desarrollo de iconos y timeline

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,177 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBeer, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+
+const Docs = () => {
+  return (
+    <div className="container w-full min-h-screen p-4 mx-auto bg-red-200">
+      <div className="flex">
+        {/*estado inicial inactivo*/}
+        <div className='flex items-center justify-center w-[32px] h-[32px] bg-gray-200 rounded-full'>
+          1
+        </div>
+        {/*estado inicial activo*/}
+        <div className='flex items-center justify-center w-[32px] h-[32px] rounded-full border border-emerald-400' style={{backgroundColor: "#EDFBD8",}}>
+          1
+        </div>
+        {/*estado inicial default*/}
+        <div className='w-[32px] h-[32px] bg-white rounded-full'>
+          <FontAwesomeIcon icon={faCheckCircle} style={{color: "#22c55e",}} />
+        </div>
+      </div>
+      {/*estado inicial inactivo*/}
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='flex items-center justify-center w-[32px] h-[32px] bg-gray-200 rounded-full z-10'>
+            1
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0'>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] bg-gray-200 rounded-full z-10'>
+            2
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0'>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] bg-gray-200 rounded-full z-10'>
+            3
+          </div>
+        </div>
+      </div>
+      {/*estado inicial inactivo*/}
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center">
+          {/*estado inicial activo*/}
+          <div className='flex items-center justify-center w-[32px] h-[32px] rounded-full border border-emerald-400 z-10' style={{backgroundColor: "#EDFBD8",}}>
+            1
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0'>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] bg-gray-200 rounded-full z-10'>
+            2
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0'>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] bg-gray-200 rounded-full z-10'>
+            3
+          </div>
+        </div>
+      </div>
+      {/*estado inicial inactivo*/}
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center">
+          {/*estado inicial default*/}
+          <div className='w-[32px] h-[32px] bg-white rounded-full z-10'>
+            <FontAwesomeIcon icon={faCheckCircle} style={{color: "#22c55e",}} />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0' style={{backgroundColor: "#EDFBD8",}}>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] rounded-full border border-emerald-400 z-10' style={{backgroundColor: "#EDFBD8",}}>
+            2
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0'>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] bg-gray-200 rounded-full z-10'>
+            3
+          </div>
+        </div>
+      </div>
+      {/*estado inicial inactivo*/}
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center">
+          {/*estado inicial default*/}
+          <div className='w-[32px] h-[32px] bg-white rounded-full z-10'>
+            <FontAwesomeIcon icon={faCheckCircle} style={{color: "#22c55e",}} />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0' style={{backgroundColor: "#EDFBD8",}}>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] rounded-full border border-emerald-400 z-10' style={{backgroundColor: "#EDFBD8",}}>
+            2
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0'>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] bg-gray-200 rounded-full z-10'>
+            3
+          </div>
+        </div>
+      </div>
+      {/*estado inicial inactivo*/}
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center">
+          {/*estado inicial default*/}
+          <div className='w-[32px] h-[32px] bg-white rounded-full z-10'>
+            <FontAwesomeIcon icon={faCheckCircle} style={{color: "#22c55e",}} />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0' style={{backgroundColor: "#EDFBD8",}}>
+          </div>
+          <div className='w-[32px] h-[32px] bg-white rounded-full z-10'>
+            <FontAwesomeIcon icon={faCheckCircle} style={{color: "#22c55e",}} />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0' style={{backgroundColor: "#EDFBD8",}}>
+          </div>
+          <div className='flex items-center justify-center w-[32px] h-[32px] rounded-full border border-emerald-400 z-10' style={{backgroundColor: "#EDFBD8",}}>
+            3
+          </div>
+        </div>
+      </div>
+      {/*estado inicial inactivo*/}
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center">
+          {/*estado inicial default*/}
+          <div className='w-[32px] h-[32px] bg-white rounded-full z-10'>
+            <FontAwesomeIcon icon={faCheckCircle} style={{color: "#22c55e",}} />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0' style={{backgroundColor: "#EDFBD8",}}>
+          </div>
+          <div className='w-[32px] h-[32px] bg-white rounded-full z-10'>
+            <FontAwesomeIcon icon={faCheckCircle} style={{color: "#22c55e",}} />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          {/*estado inicial inactivo*/}
+          <div className='items-center justify-center w-[180px] h-[3px] bg-gray-200 -ml-0.5 -mr-0.5 z-0' style={{backgroundColor: "#EDFBD8",}}>
+          </div>
+          <div className='w-[32px] h-[32px] bg-white rounded-full z-10'>
+            <FontAwesomeIcon icon={faCheckCircle} style={{color: "#22c55e",}} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Docs;
